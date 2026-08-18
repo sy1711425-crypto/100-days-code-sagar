39535d651880b14885e66bf6d7463cc30a716b0a
sy1711425-crypto <sy1711425@gmail.com>
2026-08-18 19:33:49 +0530

    Add files via upload

---
+++ day9ques1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c, d, root1, root2;
+
+    printf("Enter coefficient a: ");
+    scanf("%f", &a);
+    printf("Enter coefficient b: ");
+    scanf("%f", &b);
+    printf("Enter coefficient c: ");
+    scanf("%f", &c);
+
+    d = (b * b) - (4 * a * c);
+
+    if (d >= 0) {
+        root1 = (-b + sqrt(d)) / (2 * a);
+        root2 = (-b - sqrt(d)) / (2 * a);
+        printf("root1 is %f\n", root1);
+        printf("root2 is %f\n", root2);
+    } else {
+        printf("roots are imaginary\n");
+    }
+
+    return 0;
+}
